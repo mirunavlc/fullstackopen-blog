440d63df60b9c9ad2f8152c02cacee2365cca9b7
rzamfiriu <rzamfiriu@barracuda.com>
2022-09-30 00:22:19 +0300

    Ex.4.5 of part4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const max = Math.max(...blogs.map((blog) => blog.likes));
+  const favorite = blogs.find((blog) => blog.likes === max);
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
