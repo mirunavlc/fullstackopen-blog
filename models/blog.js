9bab22124b8571e924b652393e73c264b362eb25
rzamfiriu <rzamfiriu@barracuda.com>
2022-09-29 22:42:24 +0300

    Ex.4.2 of part4

---
+++ models/blog.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const config = require("../utils/config");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+module.exports = Blog;
