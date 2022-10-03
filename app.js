9c4f254f0c59a589c18e8ad96365a7e51eb2499c
rzamfiriu <rzamfiriu@barracuda.com>
2022-10-03 23:27:50 +0300

    Ex.418-4.19 of part 4

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const errorHandler = require("./middleware/error");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs/", blogsRouter);
 app.use("/api/users/", usersRouter);
+app.use("/api/login/", loginRouter);
 
 app.use(errorHandler);
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,6 @@
 const blogsRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
@@ -46,17 +48,31 @@ blogsRouter.put("/:id", async (request, response, next) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response, next) => {
-  const body = request.body;
-  const user = await User.findOne({});
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user.id,
-  });
   try {
+    const body = request.body;
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+
+    const user = await User.findById(decodedToken.id);
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user.id,
+    });
     const savedBlog = await blog.save();
 
     user.blogs = user.blogs.concat(savedBlog._id);
